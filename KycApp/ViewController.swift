8e1a4a9797689a9e53a38f4a01c661a191439f89
mohammedibrahim-mi <34709179+mohammedibrahim-mi@users.noreply.github.com>
2020-12-11 10:45:10 +0530

    Initial Commit

---
+++ KycApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KycApp
+//
+//  Created by Pixel on 11/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
